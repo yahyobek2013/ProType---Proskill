4a4bcf8aca459ea6e1bcda5c9e0b4774778e1b75
your GitHub username <yahyobekyaqubboyev@gmail.com>
2026-08-04 14:25:10 +0500

    Update Django settings

--- config/settings.py
+++ config/settings.py
@@ -70,10 +70,12 @@ ASGI_APPLICATION = 'config.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.environ.get("SQLITE_PATH", BASE_DIR / "db.sqlite3"),
     }
 }
 
